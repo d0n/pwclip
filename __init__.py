1136b6452bf3064a4fb9e51fa9db82d44153acbd
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-06-05 21:08:10 +0200

    { Modified: [33m__init__.py[0m, Added: [33mdaemon.py[0m } [35mmaster[0m

--- __init__.py
+++ __init__.py
@@ -1,7 +1,14 @@
-__version__ = '0.1'
-from .crontools import stamp, fileage
-from .misctools import which, random, string2bool
-from .pathtools import bestlim, lineno, realpaths, confpaths, confdats
-from .users import Users
 
+__version__ = '0.1'
+
+from .crontools import stamp, fileage
+
+from .misctools import which, random, string2bool
+
+from .pathtools import bestlim, lineno, realpaths, confpaths, confdats
+
+from .users import Users
 user = Users()
+
+from .daemon import Daemon
+
