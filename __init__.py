12c5c89e93283e7570660ca1d2112aa0fcbbb2bf
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-06-22 04:00:28 +0200

    { Modified: [33m__init__.py[0m } [35mmaster[0m

--- __init__.py
+++ __init__.py
@@ -6,8 +6,6 @@ from .misctools import which, random, string2bool
 
 from .pathtools import bestlim, lineno, realpaths, confpaths, confdats
 
-from .pkgtools import DePyKG, Apytude
-
 from .users import Users
 user = Users()
 
