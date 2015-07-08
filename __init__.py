0e6b97befeaf24b946db156d41a08d7787d0708e
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-07-08 04:59:12 +0200

    { Modified: [33m__init__.py[0m } [35mmaster[0m

--- __init__.py
+++ __init__.py
@@ -1,19 +1,8 @@
-# stdlib imports
-if __package__ == 'accmw':
-	from .csr import csrgen
-
-	from .blogger import BreitLogger
-
-	from .misc import listhosts, listclusters, jolofix
-
-	from .puppet import Puppet
-
-	from .keyverteiler import kvntool
-
-	from .ndcli import ndcli
-
-	from .deborphan import orphan
-
-	from .unitix import UnitixUsers
-else:
-	print(__package__)
+from .csr import csrgen
+from .blogger import BreitLogger
+from .misc import listhosts, listclusters, jolofix
+from .puppet import Puppet
+from .keyverteiler import kvntool
+from .ndcli import ndcli
+from .deborphan import orphan
+from .unitix import UnitixUsers
