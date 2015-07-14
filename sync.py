c89ef6ef620bf38556e96c73899c43c71d1faf94
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-07-14 21:08:39 +0200

    {'modified': ['sync.py']}

--- sync.py
+++ sync.py
@@ -52,7 +52,9 @@ class RepoSync(GitSync):
 			if (os.path.isdir('%s/.git'%repo) or \
 				  os.path.isfile('%s/.gitmodules'%repo) or \
 				  os.path.isfile('%s/.git'%repo)):
-				return self.genmessage(self.gitsync())
+				commits = self.gitsync()
+				for branch in commits:
+					return self.genmessage(commits[branch])
 			elif os.path.isdir(repo+'/.svn'):
 				return self.stdx('svn up')
 
