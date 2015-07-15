c3347034996be08bef7af5072c2de4f24142b292
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-07-16 01:50:05 +0200

    {'modified': ['git.py']}

--- git.py
+++ git.py
@@ -72,6 +72,8 @@ class GitRepo(Command):
 			c-=1
 
 	def _fetch_(self, fetchall=None):
+		if self.dbg:
+			print(self._fetch)
 		command = '%s fetch'%self.gitbin
 		if fetchall:
 			command = '%s fetch --all'%self.gitbin
@@ -79,18 +81,24 @@ class GitRepo(Command):
 			return True
 
 	def _fetchref_(self):
+		if self.dbg:
+			print(self._fetchref_)
 		fetchead = '%s/FETCH_HEAD'%self.gitdir
 		if os.path.isfile(fetchead):
 			with open(fetchead, 'r') as f:
 				return f.read().split('\t')[0].strip()
 
 	def _headref_(self):
+		if self.dbg:
+			print(self._headref_)
 		hrefile = '%s/refs/heads/%s'%(self.gitdir, self._head())
 		if os.path.isfile(hrefile):
 			with open(hrefile, 'r') as f:
 				return f.read().split(' ')[0].strip()
 
 	def _heads(self):
+		if self.dbg:
+			print(self._heads)
 		if not os.path.exists('%s/refs/heads'%(self.gitdir)):
 			errmsg = 'no basedir was set and current one is no git repo %s'%(
                 self.gitdir)
@@ -98,10 +106,14 @@ class GitRepo(Command):
 		return os.listdir('%s/refs/heads'%(self.gitdir))
 
 	def _head(self):
+		if self.dbg:
+			print(self._head)
 		with open('%s/HEAD'%(self.gitdir), 'r') as f:
 			return f.read().split('/')[-1].strip()
 
 	def _remoteref(self):
+		if self.dbg:
+			print(self._remoteref)
 		self._fetch_(fetchall=True)
 		gitdir = self.gitdir
 		remref = '%s/refs/remotes/origin/%s'%(gitdir, self._head())
@@ -111,18 +123,26 @@ class GitRepo(Command):
 			return f.read().strip()
 
 	def _remotes(self):
+		if self.dbg:
+			print(self._remotes)
 		return [rem for rem in os.listdir(
 	        '%s/refs/remotes/origin/'%(self.gitdir)) if rem != 'HEAD']
 
 	def _isbehind(self):
+		if self.dbg:
+			print(self._isbehind)
 		if self._remoteref() != self._headref_():
 			return True
 
 	def _isahead(self):
+		if self.dbg:
+			print(self._isahead)
 		if self._headref_() != self._fetchref_():
 			return True
 
 	def checkout(self, branch, *files):
+		if self.dbg:
+			print(self.checkout)
 		heads = self._heads()
 		if files:
 			command = '%s checkout %s %s'%(
@@ -135,10 +155,14 @@ class GitRepo(Command):
 		return self.erno(command)
 
 	def pull(self, origin='origin'):
+		if self.dbg:
+			print(self.pull)
 		command = '%s pull %s' %(self.gitbin, origin)
 		return int(self.call(command))
 
 	def push(self, remote=None, origin='origin', setup=None):
+		if self.dbg:
+			print(self.push)
 		if not remote:
 			remote = self._head()
 		command = '%s push %s %s'%(self.gitbin, origin, remote)
@@ -148,16 +172,22 @@ class GitRepo(Command):
 		return int(self.call(command))
 
 	def add(self, *files):
+		if self.dbg:
+			print(self.add)
 		command = '%s add -A'%(self.gitbin)
 		if files:
 			command = '%s add %s'%(self.gitbin, ' '.join(f for f in files))
 		return int(self.call(command))
 
 	def commit(self, message):
+		if self.dbg:
+			print(self.commit)
 		command = '%s commit -a -m "%s"' %(self.gitbin, message)
 		return int(self.call(command))
 
 	def commitstamp(self, rpofile):
+		if self.dbg:
+			print(self.commitstamp)
 		rpofile = realpaths(rpofile)
 		stamp = self.stdo('%s log -1 --format=%%at %s'%(self.gitbin, rpofile))
 		if stamp:
@@ -165,12 +195,16 @@ class GitRepo(Command):
 			return int(stamp)
 
 	def show(self, *args, **kwargs):
+		if self.dbg:
+			print(self.show)
 		cmd = '%s show' %(self.gitbin)
 		if 'commitstamp' in args:
 			cmd = '%s show -s --format=%%at' %(self.gitbin)
 		return self.stdo(cmd).strip()
 
 	def gitstatus(self, mode='--porcelain'):
+		if self.dbg:
+			print(self.gitstatus)
 		out = self.stdx('%s status %s'%(self.gitbin, mode))
 		if not out or mode != '--porcelain':
 			return out
@@ -202,8 +236,9 @@ class GitRepo(Command):
 			return stats
 
 	def genmessage(self, stats=None):
+		if self.dbg:
+			print(self.genmessage)
 		if not stats:
-			print(stats)
 			stats = self.gitstatus()
 			if not stats:
 				return
