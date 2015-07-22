063f993f4e57a861dff6fb1aa50d6e98c976a1d1
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2015-07-22 14:50:45 +0200

    {'modified': ['firstuser.py']}

--- firstuser.py
+++ firstuser.py
@@ -2,7 +2,10 @@
 def firstuser(uid='1000', gid=None): #, pattern=None):
 	gid = gid if gid else uid
 	with open('/etc/passwd', 'r') as pwd:
-		return [
+		users = [
                  l.split(':')[0] for l in pwd.readlines() \
                  if l.split(':')[2] == uid and l.split(':')[3] == gid
                 ]
+	if len(users) > 1:
+		print('user could not be distinctly determined', file=sys.stderr)
+	return users[0]
