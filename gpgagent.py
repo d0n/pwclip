7f4a4b57976495bfcbd34503e7ff187795dfa3a3
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-08-10 19:50:37 +0200

    {'modified': ['gpgagent.py']}

--- gpgagent.py
+++ gpgagent.py
@@ -1,5 +1,5 @@
 def agentinfo(user):
 	agentinfo = '/home/%s/.gnupg/gpg-agent.info'%user
 	with open(agentinfo, 'r') as agi:
-		return dict((k.strip(), v.strip()) for (k, v) in dict(line.split('=') for line in agi.readlines()).items())
+		return dict((k.split(';')[0].strip(), v.split(';')[0].strip()) for (k, v) in dict(line.split('=') for line in agi.readlines()).items())
 
