3722848129a09d82e23ea7bf4d89efc1d965c224
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2015-12-15 01:23:05 +0100

    {'modified': ['gpgagent.py']}

--- gpgagent.py
+++ gpgagent.py
@@ -3,7 +3,8 @@ def agentinfo(user):
 	agentinfo = '/home/%s/.gnupg/agent.env'%user
 	try:
 		with open(agentinfo, 'r') as agi:
-			return dict((k.split(';')[0].strip(), v.split(';')[0].strip()) for (k, v) in dict(line.split('=') for line in agi.readlines()).items())
+			return dict(line.split(';')[0].split('=') for line in agi.readlines())
+			#return dict((k.split(';')[0].strip(), v.split(';')[0].strip()) for (k, v) in dict(line.split('') for line in agi.readlines()).items())
 	except FileNotFoundError as err:
 		print(err, file=_stderr)
 
