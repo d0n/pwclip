7f32618ea17a417466bbb4ee7666f82cc9512bcd
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-01-04 00:17:55 +0100

    {'modified': ['__init__.py', 'colortext.py']}

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-from .colortext import *
-__all__ = [
-    'blu', 'bblu', 'cya', 'bcya', 'gre', 'bgre', 'grn',
-    'bgrn', 'red', 'bred', 'vio', 'bvio', 'whi', 'bwhi',
-    'yel', 'byel', 'abort', 'error', 'fatal', 'tabd']
+"""init of colortext just import all usability functions"""
+from .colortext import \
+    blu, bblu, cya, bcya, \
+    gre, bgre, grn, bgrn, \
+    red, bred, vio, bvio, \
+    whi, bwhi, yel, byel, \
+    abort, error, fatal, tabd
--- colortext.py
+++ colortext.py
@@ -1,9 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+text colorisation functions - due to extendet use of the python3 print
+function this is for python3 only
+"""
 from sys import \
     stderr as _stderr
 
 # get color escape sequence from string
 def __colorize(color, text):
+	"""color-tag prepending and end-tag appending function"""
 	string = '\033['
 	if len(color) == 4:
 		color = color[1:]
@@ -31,57 +37,73 @@ def __colorize(color, text):
 # one for normal and one for bold text
 
 def blu(text):
+	"""function for color blue"""
 	#import sys
 	#func = sys._getframe().f_code.co_name
 	return __colorize('blu', text)
 def bblu(text):
+	"""function for color bold-blue"""
 	return __colorize('bblu', text)
 
 def cya(text):
+	"""function for color cyan"""
 	return __colorize('cya', text)
 def bcya(text):
+	"""function for color bold-cyan"""
 	return __colorize('bcya', text)
 
 def gre(text):
+	"""function for color grey"""
 	return __colorize('gre', text)
 def bgre(text):
+	"""function for color bold-grey"""
 	return __colorize('bgre', text)
 
 def grn(text):
+	"""function for color green"""
 	return __colorize('grn', text)
 def bgrn(text):
+	"""function for color bold-green"""
 	return __colorize('bgrn', text)
 
 def red(text):
+	"""function for color red"""
 	return __colorize('red', text)
 def bred(text):
+	"""function for color bold-red"""
 	return __colorize('bred', text)
 
 def vio(text):
+	"""function for color violet"""
 	return __colorize('vio', text)
 def bvio(text):
+	"""function for color bold-violet"""
 	return __colorize('bvio', text)
 
 def whi(text):
+	"""function for color white"""
 	return __colorize('whi', text)
 def bwhi(text):
+	"""function for color bold-white"""
 	return __colorize('bwhi', text)
 
 def yel(text):
+	"""function for color guess what? - yellow ;)"""
 	return __colorize('yel', text)
 def byel(text):
+	"""function for color and you already guessed it... bold-yellow"""
 	return __colorize('byel', text)
 
 # functions for some high frequent use cases:
 def abort(*messages):
-	if not messages:
-		messages = ('\naborted by keystroke', )
-	'''
+	"""
 	prints all text in blu by using STDOUT but also kills its
 	parent processes and returns 0 (OK) instead of 1 (ERROR)
 	by default used for aborting on STRG+C (see message,
 	for "KeyboardInterrupt" exceptions)
-	'''
+	"""
+	if not messages:
+		messages = ('\naborted by keystroke', )
 	msgs = []
 	for msg in messages:
 		if (messages.index(msg) % 2) == 0:
@@ -113,7 +135,7 @@ def error(*args, **kwargs):
 		else:
 			msgs.append(yel(arg))
 	print(' '.join(msg for msg in msgs), flush=True, file=_stderr)
-		
+
 
 
 def fatal(*args, **kwargs):
@@ -136,11 +158,20 @@ def fatal(*args, **kwargs):
 	print(' '.join(msg for msg in msgs), flush=True, file=_stderr)
 	exit(1)
 
-def tabd(keyvals):
-	if not type(keyvals) is dict:
+def tabd(keyvals, add=2):
+	"""
+	this is a function where i try to guess the best indentation for text
+	representation of key-value paires with best matching indentation
+	e.g:
+	foo         = bar
+	a           = b
+	blibablubb  = bla
+	^^indent "bar" and "b" as much as needed ("add" is added to each length)
+	"""
+	if isinstance(keyvals, dict):
 		raise TypeError('cannot process type %s expected dict'%type(keyvals))
 	tabbed = ''
-	lim = max(len(k) for k in keyvals.keys())+4
+	lim = max(len(k) for k in keyvals.keys())+int(add)
 	for (key, val) in sorted(keyvals.items()):
 		tabbed = '%s%s%s= %s\n'%(tabbed, key, ' '*int(lim-len(key)), val)
 	return tabbed.strip()
