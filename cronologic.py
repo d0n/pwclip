8a2505f683ec04b5822ead419d4e6a004eaf0dbb
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2016-01-13 13:37:57 +0100

    {'modified': ['cronologic.py']}

--- cronologic.py
+++ cronologic.py
@@ -10,15 +10,15 @@ from datetime import \
     datetime as _datetime
 
 def stamp():
-    now = datetime()._now()
-    stamp = '%s.%s'%(now.date(), now.time().split('.')[0])
-    return stamp
+	now = _datetime.now()
+	stamp = '%s.%s'%(now.date(), str(now.time()).split('.')[0])
+	return stamp
 
 def fileage(trgfile):
-    timefile = '/tmp/thetime'
-    trgtime = _stat(trgfile).st_mtime
-    with open(timefile, 'w+'):
-        thetime = _stat(timefile).st_mtime
-    os.remove(timefile)
-    return int(str(((thetime-trgtime))).split('.')[0])
+	timefile = '/tmp/thetime'
+	trgtime = _stat(trgfile).st_mtime
+	with open(timefile, 'w+'):
+		thetime = _stat(timefile).st_mtime
+	os.remove(timefile)
+	return int(str(((thetime-trgtime))).split('.')[0])
 
