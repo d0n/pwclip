36eb0423cf68bebb8f9e6ec60ae89ededae7c307
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-01-14 02:35:31 +0100

    {'added': ['cron.py'], 'modified': ['__init__.py', 'which.py'], 'deleted': ['cronologic.py']}

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.1'
 
-from .cronologic import stamp, fileage
+from .cron import stamp, fileage
 
 from .path import absrelpath, realpaths, confpaths, confdats, jconfdats
 
--- which.py
+++ which.py
@@ -1,6 +1,9 @@
-import os
-def which(prog):
-	for path in os.environ['PATH'].split(':'):
-		if os.access('%s/%s'%(path, prog), os.X_OK):
-			return '%s/%s'%(path, prog)
+from os import \
+    X_OK as _XOK, \
+    access as _access,\
+    environ as _environ
 
+def which(prog):
+	for path in _environ['PATH'].split(':'):
+		if _access('%s/%s'%(path, prog), _XOK):
+			return '%s/%s'%(path, prog)
