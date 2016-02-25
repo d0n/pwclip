03935d0cf4699173dda9c73b6e81433f2adc3902
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2016-02-25 16:50:01 +0100

    { Modified: [33mrandom.py[0m } [35mmaster[0m

--- random.py
+++ random.py
@@ -1,16 +1,14 @@
-from re import search as _search, U as __U__
+from re import search as _search, U as _U, A as _A
 from os import urandom as _urandom
 
 def random(limit=10, pattern='[\w -~]'):
 	retstr = ''
 	while len(retstr) < int(limit):
 		try:
-			out = _urandom(1)
-			out = out.decode()
+			out = _urandom(1).decode()
 		except UnicodeDecodeError as err:
 			continue
-		char = _search(r'%s'%(pattern), out, __U__)
-		if char:
+		if out and _search(r'%s'%(pattern), out):
 			retstr = '%s%s'%(retstr, out.strip())
 	return retstr
 
