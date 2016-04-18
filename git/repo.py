b954edc104342eefccae40135798277e9cdfa0bf
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-04-18 19:42:47 +0200

    {'modified': ['git/repo.py']}

--- git/repo.py
+++ git/repo.py
@@ -196,8 +196,11 @@ class GitRepo(Command):
 		stats, ermsg, ernum = self.oerc(
             '%s status -b --porcelain'%self.gitbin)
 		if ermsg or ernum != 0:
-			error('`git status` exited with errorcode %d and message:\n%s'%(
-                ernum, ermsg))
+			error(
+                'command', 'git status',
+                'exited with errorcode',
+                '%d'%ernum, 'and message:\n',
+                '%s'%ermsg)
 		stats = stats.split('\n')
 		ablines = [l for l in stats if l.startswith('##')]
 		anum, bnum = 0, 0
