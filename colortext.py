0aa942f9475dafe9f0dd55bbb5b13c8a5ecccf56
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2016-05-02 07:13:54 +0200

    {'modified': ['colortext.py']}

--- colortext.py
+++ colortext.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 text colorisation functions - due to extendet use of the python3 print
@@ -178,35 +179,47 @@ def tabd(keyvals, add=2):
 		tabbed = '%s%s%s= %s\n'%(tabbed, key, ' '*int(lim-len(key)), val)
 	return tabbed.strip()
 
-def didict(keyvals, delimiter='=', indent=2):
-	lim = max(len(k) for k in keyvals.keys())+int(indent)
-	distr = ''
-	for (key, val) in sorted(keyvals.items()):
-		distr = '%s%s%s= %s\n'%(distr, key, ' '*int(lim-len(key)), val)
-	return distr.strip()
 
-
-def pprind(*strs, **strdic):
-	"""
-	universal pretty printing with custom or auto delimiter
-	"""
-	if '__color__' in strdic.keys():
-		color = strdic['__color__']
-	if '__delim__' in strdic.keys():
-		delim = strdic['__delim__']
-	if '__indin__' in strdic.keys():
-		indin = strdic['__indin__']
-	if strdic:
-		print(didict(strdic))
-	
-
-
-
-
-
-
-
-
+class Debugger(object):
+	_dbg = False
+	_vrb = False
+	_logfile = ''
+	@property                # dbg <bool>
+	def dbg(self):
+		return self._dbg
+	@dbg.setter
+	def dbg(self, val):
+		self._dbg = True if val else False
+	@property                # vrb <bool>
+	def vrb(self):
+		return self._vrb
+	@vrb.setter
+	def vrb(self, val):
+		self._vrb = True if val else False
+	@property                # logfile <str>
+	def logfile(self):
+		return self._logfile
+	@logfile.setter
+	def logfile(self, val):
+		self._logfile = val if not val.startswith('~') else _expanduser(val)
+		if not _isdir(_dirname(val)):
+			raise FileNotFoundError(
+                '"%s" no such file or directory'%_dirname(val))
+	def debug(self, message='', debug=None, verbose=None):
+		dbg = debug if debug is not None else self.dbg
+		vrb = verbose if verbose is not None else self.vrb
+		if dbg:
+			__stack = '%s: %s'%(_stack()[-1][1], str(_stack()[2]).strip('()'))
+			if message:
+				message = message if not vrb else '%s << %s >>'%(
+                    __stack, message)
+			message = __stack if not message else message
+			if self.logfile:
+				with open(self.logfile, 'r') as log:
+					log.write(message)
+				if not vrb:
+					return
+			print(bgre(message))
 
 
 
