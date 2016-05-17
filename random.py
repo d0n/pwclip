7683513bfed396a4ba12819e3097937678282f99
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-05-18 00:13:13 +0200

    {'modified': ['random.py']}

--- random.py
+++ random.py
@@ -1,14 +1,17 @@
+from sys import stdout as _stdout
 from re import search as _search
 from os import urandom as _urandom
 
-def random(limit=10, pattern='[\w -~]'):
+def random(limit=10, regex='[\w -~]'):
 	retstr = ''
-	while len(retstr) < int(limit):
+	while True:
 		try:
 			out = _urandom(1).decode()
-		except UnicodeDecodeError as err:
+		except UnicodeDecodeError:
 			continue
-		if out and _search(r'%s'%(pattern), out):
+		if out and _search(regex, out):
 			retstr = '%s%s'%(retstr, out.strip())
+		if len(retstr) >= int(limit):
+			break
 	return retstr
 
