b63afc6bb0c03b1ff0df07a4a804cc4b3a5bc9fe
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2016-05-18 04:29:44 +0200

    {'modified': ['executor.py']}

--- executor.py
+++ executor.py
@@ -21,9 +21,8 @@ class Command(object):
 	_sh_ = True
 	_su_ = False
 	_dbg = False
-	_tout_ = 10
+	_tout_ = None
 	def __init__(self, *args, **kwargs):
-		
 		for arg in args:
 			arg = '_%s'%(arg)
 			if hasattr(self, arg):
