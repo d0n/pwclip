9ff42725b6994997965fb68871078880e6362884
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-06-11 20:14:15 +0200

    { Deleted: [33mrexecutor.py[0m, Modified: [33m__init__.py[0m [33mexecutor.py[0m } [35mjoda-v0.2[0m

--- __init__.py
+++ __init__.py
@@ -1,10 +1,1 @@
-import sys
-from os.path import dirname as _dirname, abspath as _abspath, \
-    isdir as _isdir, basename as _basename
-
-__miscdir__ = _abspath('%s/..'%_dirname(__file__))
-if _isdir(__miscdir__):
-	sys.path = [__miscdir__] + [p for p in sys.path if p != __miscdir__]
-
-from .executor import Command, command, sucommand, sudofork
-from .rexecutor import SSHCommand
+from .executor import Command
