2d52fccaa566339dae641517c601c9d4bf87efcd
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-06-24 08:42:46 +0200

    {'modified': ['debugger.py']}

--- debugger.py
+++ debugger.py
@@ -7,24 +7,46 @@ from yaml import \
     load as _load
 import logging
 
-def debug():
+def logger(lvl='INFO'):
+	"""
+	CRITICAL: 50
+	ERROR:    40
+	WARNING:  30
+	INFO:     20
+	DEBUG:    10
+	NOTSET:   0
+	"""
 	prog = _basename(_argv[0])
 	name = prog.split('.')[0]
 	stamp = '%F.%T'
 	logln = '%(asctime)s,%(msecs)d %(name)s:%(levelname)s:%(funcName)s %(message)s'
 	outln = '%(asctime)s %(name)s: %(message)s'
 	_file = _expanduser('~/log/%s.log'%name)
+	if lvl == ('CRITICAL', 50):
+		lvl = logging.CRITICAL
+	elif lvl in ('DEBUG', 10):
+		lvl = logging.DEBUG
+	elif lvl == ('ERROR', 40):
+		lvl = logging.ERROR
+	elif lvl in ('INFO', 20):
+		lvl = logging.INFO
+	elif lvl in ('NOTSET', None, 0):
+		lvl = logging.NOTSET
+	elif lvl in ('WARNING', 30):
+		lvl = logging.WARNING
 	log = logging.getLogger(name)
-	log.setLevel(logging.DEBUG)
-
+	log.setLevel(lvl)
 	ch = logging.StreamHandler()
-	ch.setLevel(logging.DEBUG)
+	ch.setLevel(lvl)
 	ch.setFormatter(logging.Formatter(outln, datefmt=stamp))
-
 	fh = logging.FileHandler(_file)
-	fh.setLevel(logging.DEBUG)
+	fh.setLevel(lvl)
 	fh.setFormatter(logging.Formatter(logln, datefmt=stamp))
-
 	log.addHandler(ch)
 	log.addHandler(fh)
+	return log
+
+
+def debug():
+	log = logger()
 	return log.debug
