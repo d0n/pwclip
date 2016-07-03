41b606693b2d2edad4b59b5ad9a0b435ab34b029
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-07-03 23:24:00 +0200

    {'modified': ['d0nkey.py']}

--- d0nkey.py
+++ d0nkey.py
@@ -3,17 +3,26 @@
 d0nkey - yubikey module
 """
 import sys
+from os import environ
 from yubico import \
     find_yubikey, yubikey, \
     yubico_exception
 
+from yubico.yubikey_usb_hid import YubiKeyHIDDevice
+
+from yubico.yubikey_neo_usb_hid import YubiKeyNEO_USBHID
+
+from yubico.yubikey_4_usb_hid import YubiKey4_USBHID
+
 from binascii import hexlify
 
-def _yubikeys(dbg=False, ykser=None):
+from pyperclip import copy as copyclip
+
+def _yubikeys(ykser=None, dbg=None):
 	"""
 	return a list of yubikeys objects
 	"""
-	keys = []
+	keys = {}
 	for i in range(0, 255):
 		try:
 			key = find_yubikey(debug=dbg, skip=i)
@@ -21,19 +30,31 @@ def _yubikeys(dbg=False, ykser=None):
 			break
 		if ykser and int(ykser) != int(key.serial()):
 			continue
-		yield key
+		keys[key.serial()] = key
+	return keys
 
 def _slotchalres(yk, chal, slot):
 	try:
 		return hexlify(yk.challenge_response(
-			chal.ljust(64, '\0').encode(), slot=slot)).decode()
+            chal.ljust(64, '\0').encode(), slot=slot)).decode()
 	except yubico_exception.YubicoError as err:
 		pass
 
-def _chalres(chal, slot=None, ykser=None):
+def chalres(chal, slot=2, ykser=None):
+	keys = _yubikeys(ykser)
+	for (ser, key) in keys.items():
+		return _slotchalres(key, chal, slot)
+	"""
 	if not slot:
 		return [_slotchalres(k, chal, s) for k in _yubikeys(ykser=ykser) for s in (2, 1)]
 	res = [_slotchalres(k, chal, slot) for k in _yubikeys(ykser=ykser)]
 	return res if len(res) > 1 else res[0]
+	"""
 
-
+def cpchalres(chal=None, slot=2, ykser=None):
+	if not ykser:
+		ykser = '' if 'YKSERIAL' not in environ.keys() else environ['YKSERIAL']
+	if not chal:
+		chal = input('enter challenge: ')
+	__res = chalres(chal, slot, ykser)
+	copyclip(__res)
