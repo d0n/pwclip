35174b5c8de5d859d158546ea7a81e08f2a470b6
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2016-07-11 18:34:14 +0200

    {'modified': ['timsatan.py']}

--- timsatan.py
+++ timsatan.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-from os import \
-    makedirs as _makedirs
-
 from os.path import \
     expanduser as _expanduser
 
@@ -36,19 +33,17 @@ class LoginFailedError(Exception): pass
 class TimeSatan(object):
 	_dbg = False
 	browser = None
-	_sesurl = 'https://login.1and1.org/ims-sso/login?service=' \
+	url = 'https://login.1and1.org/ims-sso/login?service=' \
         'http%3A%2F%2Ftimsato.tool.1and1.com%2Fxml%2Fenter%2Feffort'
 	username = userfind()
 	__passwd = None
 	casslpem = _expanduser('~/.config/catrust/ssl.pem')
-	cache = _expanduser('~/.cache/timsatan')
 	def __init__(self, *args, **kwargs):
 		self._dbg = True if 'dbg' in args else self._dbg
 		self.username = self.username if (
             'username' not in kwargs.keys()) else kwargs['username']
 		self.casslpem = self.casslpem if (
-            'casslpem' not in kwargs.keys()) else kwargs['casslpem']
-		self._mkcache_()
+		    'casslpem' not in kwargs.keys()) else kwargs['casslpem']
 		if self.dbg:
 			lim = int(max(len(k) for k in TimeSatan.__dict__.keys()))+4
 			print(bgre('%s\n%s\n\n%s\n%s\n'%(
@@ -69,24 +64,20 @@ class TimeSatan(object):
 	def dbg(self, val):
 		self._dbg = val
 
-	@property                # sesurl <str>
-	def sesurl(self):
-		try:
-			with open('%s/timsatan.ses'%self.cache, 'r') as tsh:
-				return tsh.read()
-		except FileNotFoundError:
-			return self._sesurl
-	@sesurl.setter
-	def sesurl(self, val):
-		with open('%s/timsatan.ses'%self.cache, 'w+') as tsh:
-			tsh.write(self._sesurl)
+	def _sesurl(self, session, what):
+		url = 'https://timsato.tool.1and1.com/xml/'
 
 	@staticmethod
-	def _mkcache_():
-		try:
-			_makedirs(_expanduser('~/.cache/timsatan'))
-		except FileExistsError:
-			pass
+	def _session(url, sesfile='~/.cache/timsatan/timsatan.session'):
+		sesfile = _expanduser(sesfile)
+		if 'psessionid=' in url:
+			with open(sesfile, 'w+') as sfh:
+				ses = url.split('psessionid=')[1].split('/')[0]
+				sfh.write(ses)
+			return ses
+		elif fileage(sesfile) <= 3600:
+			with open(sesfile, 'r') as fsh:
+				return sfh.read()
 
 	def __login_(self):
 		if self.dbg:
@@ -98,9 +89,9 @@ class TimeSatan(object):
 		self.browser = _build_opener(
             _HTTPCookieProcessor(cj),
             _HTTPSHandler(debuglevel=0,context=cxt))
-		tree = _html.fromstring(self.browser.open(self.sesurl).read())
+		tree = _html.fromstring(self.browser.open(self.url).read())
 		form = tree.find('.//form')
-		response = self.browser.open(self.sesurl,
+		response = self.browser.open(self.url,
                 _urlencode({"username": self.username,
                     "password": self.__passwd,
                     "lt": form.find('.//input[@name="lt"]').value,
@@ -108,25 +99,25 @@ class TimeSatan(object):
                     "_eventId": form.find('.//input[@name="_eventId"]').value,
                     "submit": form.find('.//input[@name="submit"]').value
                 }).encode())
-		self.sesurl, res = response.geturl(), response.read()
-		if self.sesurl.startswith('https://login.1and1.org/'):
+		self.url, res = response.geturl(), response.read()
+		if self.url.startswith('https://login.1and1.org/'):
 			if res.find("Invalid credentials".encode()) != -1:
 				raise LoginFailedError(
                     'Login failed (invalid credentials)')
 			elif res.find("Change password".encode()) != -1:
 				print('password change required')
-				self.sesurl = self.browser.open(self.sesurl).geturl()
+				self.url = self.browser.open(self.url).geturl()
 			else:
 				print(res.decode())
 				raise LoginFailedError()
 
-	def _book(self, duration, project, task, day=None, comment=''):
+	def _book_(self, duration, project, task, day=None, comment=''):
 		if self.dbg:
 			print(bgre(self._book_))
 		day = day if day else self.curday
 		__d = tuple(int(i) for i in reversed(str(day).split('-')))
 		self.browser.open(
-            self.sesurl,
+            self.url,
             _urlencode({"__from": "%02d.%02d.%d"%__d}).encode())
 		code = _urlencode({
             "__from": "%02d.%02d.%d"%__d,
@@ -137,7 +128,7 @@ class TimeSatan(object):
             "project": project,
             "task": task,
             "comment": comment}).encode()
-		_effort = self.browser.open(self.sesurl, code)
+		_effort = self.browser.open(self.url, code)
 		response = _effort.read()
 		if response.find('Success'.encode()) == -1:
 			raise Exception(
@@ -150,8 +141,8 @@ class TimeSatan(object):
 			print(bgre('%s\n  %s'%(self.bookeffort, tabd(kwargs))))
 		self.__login_()
 		for m in {'duration', 'project','task'}:
-			assert kwargs[m] not in (None, '')
-		return self._book(**kwargs)
+			assert kwargs[m] is not None
+		return self._book_(**kwargs)
 
 
 
