2e89d1782718d32b797db7dab04f92059d8b0c27
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-08-11 19:56:58 +0200

    {'modified': ['debugger.py']}

--- debugger.py
+++ debugger.py
@@ -1,19 +1,35 @@
 import sys
-from os import getuid, makedirs
+from os import getuid
 from os.path import \
-    basename, expanduser
+    basename, expanduser, isdir
 
-import logging
+from yaml import \
+    load
+
+from logging import \
+    basicConfig, getLogger, INFO
 
 def logger(name):
 	logdir = '/var/log/%s'%name
 	if getuid() != 0:
 		logdir = expanduser('~/log/%s'%name)
 	logfile = '%s/%s.log'%(logdir, name)
-	try: makedirs(logdir)
-	except FileExistsError: pass
-	logging.basicConfig(
-        format='%(asctime)s %(name)s:%(levelname)s:%(funcName)s - %(message)s',
-        level=logging.INFO, filename=logfile, datefmt='%FT%T%:z')
-	return logging.getLogger(logfile)
+	if not isdir(logdir):
+		makedirs(logdir)
+	basicConfig(
+        format='%(asctime)s,%(msecs)03d ' \
+            '%(levelname)s %(funcName)s - %(message)s',
+        level=INFO, filename=logfile, datefmt='%F.%T')
+	return getLogger(logfile)
+
+def debug(msg, *args, **kwargs):
+    """
+    Log a message with severity 'DEBUG' on the root logger. If the logger has
+    no handlers, call basicConfig() to add a console handler with a pre-defined
+    format.
+    """
+    if len(root.handlers) == 0:
+        basicConfig()
+    msg = '\033[01;30m%s\033[0m'%msg
+    root.debug(msg, *args, **kwargs)
 
