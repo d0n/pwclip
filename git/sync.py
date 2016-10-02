509a9880507fd3d66c057b1211013f9fa4bd01bb
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-10-02 20:24:40 +0200

    {'modified': ['git/sync.py']}

--- git/sync.py
+++ git/sync.py
@@ -95,7 +95,7 @@ class GitSync(GitRepo):
 				stats = self.gitsync(branch)
 				if not stats:
 					continue
-				branchstats[_basename(repo)].update(stats)
+				branchstats[basename(repo)].update(stats)
 			if branchstats != {}:
 				repostats[rpo] = branchstats
 
