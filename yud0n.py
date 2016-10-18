b3aa188ec487115acaa5ae1826a86a5da75b2f81
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-10-18 16:47:20 +0200

    {'deleted': ['yud0n.py']}

--- yud0n.py
+++
@@ -1,8 +0,0 @@
-"""yubikey wrapper lib testing"""
-import yubico as yubi
-
-def yud0n(challenge):
-	"""yubikey testing"""
-	key = yubi.find_yubikey(debug=False)
-	chal = challenge.encode().ljust(64, b'\0')
-	return key.challenge_response(chal, slot=2)
