c2d33107db48997a00b421fde4b35a5d80d89a8f
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2016-12-27 16:13:04 +0100

    {'modified': ['gpg.py']}

--- gpg.py
+++ gpg.py
@@ -268,7 +268,7 @@ class GPGTool(object):
 				except TclError:
 					yesno = input(
                         'no passphrase or no secret key, retry? [Y/n] ')
-				if yesno is False or yesno and yesno.lower() == 'n':
+				if not yesno or yesno and yesno.lower() == 'n':
 					break
 			elif c > 1 and not self.__pin:
 				try:
