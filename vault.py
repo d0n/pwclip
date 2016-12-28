ed2fb21b47b2fa663a9d353991a1d8d003338655
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2016-12-29 00:47:37 +0100

    {'modified': ['vault.py']}

--- vault.py
+++ vault.py
@@ -112,7 +112,7 @@ class WeakVaulter(GPGTool):
 				print('%s\n  %s %s %s'%(
                     blu('syncing more recent file:'),
                     yel(src), blu('=>'), yel(trg)))
-				ssh.scpcompstats(basename(self.vault), basename(self.vault))
+				ssh.scpcompstats(basename(self.vault), self.vault)
 
 	def _clean_(self):
 		self._fixmod_()
