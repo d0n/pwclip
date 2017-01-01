50ab2f7f834730b449bc20392222a8a5790d463c
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-01-01 21:36:00 +0100

    {'modified': ['lib/net/util/ssh.py', 'lib/secrecy/vault.py', 'mod']}

--- util/ssh.py
+++ util/ssh.py
@@ -8,7 +8,7 @@ import paramiko
 from shutil import copy2
 from socket import getfqdn as fqdn
 
-from colortext import abort
+from colortext import bgre, abort, error
 
 # default vars
 __version__ = '0.1'
@@ -41,25 +41,25 @@ class SecureSHell(object):
 	def _ssh_(host, user, port=22):
 		host = fqdn(host)
 		ssh = paramiko.SSHClient()
-		#ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		try:
 			ssh.connect(host, int(port), username=user)
 		except paramiko.ssh_exception.SSHException as err:
-			#print(err, file=sys.stderr)
-			pass
+			error(err)
 		return ssh
 
 	def rstdo(self, cmd, host=None, user=None):
+		if self.dbg:
+			print(bgre(self.rstdo))
 		host = host if host else self.host
 		user = user if user else self.user
 		ssh = self._ssh_(host, user)
-		try:
-			_, out, _ = ssh.exec_command(cmd)
-		except KeyboardInterrupt:
-			abort()
+		_, out, _ = ssh.exec_command(cmd)
 		return ''.join(out.readlines())
 
 	def get(self, src, trg, host=None, user=None):
+		if self.dbg:
+			print(bgre(self.get))
 		user = user if user else self.user
 		host = host if host else self.host
 		if not (os.path.isfile(src) or os.path.isfile(trg)):
@@ -69,6 +69,8 @@ class SecureSHell(object):
 		return scp.get(src, trg)
 
 	def put(self, src, trg, host=None, user=None):
+		if self.dbg:
+			print(bgre(self.put))
 		user = user if user else self.user
 		host = host if host else self.host
 		if not (os.path.isfile(src) or os.path.isfile(trg)):
@@ -78,10 +80,11 @@ class SecureSHell(object):
 		return scp.put(src, trg)
 
 	def rcompstats(self, src, trg, host=None, user=None):
+		if self.dbg:
+			print(bgre(self.rcompstats))
 		host = host if host else self.host
 		user = user if user else self.user
 		smt = int(str(int(os.stat(src).st_mtime))[:6])
-		print(trg, host, user)
 		rmt = self.rstdo(
             'stat -c %%Y %s'%trg, host=host, user=user)
 		if rmt:
@@ -89,30 +92,41 @@ class SecureSHell(object):
 		if rmt == smt:
 			return
 		srctrg = src, '%s@%s:%s'%(user, host, trg)
-		if int(rmt) > int(smt):
+		if rmt and int(rmt) > int(smt):
 			srctrg = '%s@%s:%s'%(user, host, trg), src
 		return srctrg
 
 	def _localstamp(self, trg):
+		if self.dbg:
+			print(bgre(self._localstamp))
 		return int(os.stat(trg).st_atime), int(os.stat(trg).st_mtime)
 	
 	def _remotestamp(self, trg, host, user):
+		if self.dbg:
+			print(bgre(self._remotestamp))
 		tat = self.rstdo(
             'stat -c %%X %s'%trg, host, user)
 		tmt = self.rstdo(
             'stat -c %%Y %s'%trg, host, user)
 		if tat and tmt: return int(tat), int(tmt)
+		return None, None
 
 	def _setlstamp(self, trg, atime, mtime):
+		if self.dbg:
+			print(bgre(self._setlstamp))
 		os.utime(trg, (atime, mtime))
 
 	def _setrstamp(self, trg, atime, mtime, host, user):
+		if self.dbg:
+			print(bgre(self._setrstamp))
 		self.rstdo(
             'touch -a --date=@%s %s'%(atime, trg), host, user)
 		self.rstdo(
             'touch -m --date=@%s %s'%(mtime, trg), host, user)
 
 	def scpcompstats(self, lfile, rfile, host=None, user=None):
+		if self.dbg:
+			print(bgre(self.scpcompstats))
 		user = user if user else self.user
 		host = host if host else self.host
 		lat, lmt = self._localstamp(lfile)
@@ -121,7 +135,7 @@ class SecureSHell(object):
 		#print(rmt)
 		if rmt == lmt:
 			return
-		elif rmt > lmt:
+		elif rmt and rmt > lmt:
 			self.get(rfile, lfile, host, user)
 			self._setlstamp(lfile, rat, rmt)
 		else:
