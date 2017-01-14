8ea90260a09ff171a0af96fa3cbed72b4b31babf
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-01-14 21:44:54 +0100

    {'modified': ['lib/repo/git/sync.py']}

--- git/sync.py
+++ git/sync.py
@@ -80,7 +80,7 @@ class GitSync(GitRepo):
 				yield {repo: branchstats}
 			trees = self.gitsubtrees()
 			if trees:
-				print(blu('syncing'), yel(repo), blu('subtrees:\n\t'), '\n\t'.join(yel(t) for t in self.gitsubtrees()))
+				print(blu('syncing'), yel(repo), blu('subtrees:\n\t'), '\n \t'.join(yel(t) for t in self.gitsubtrees()))
 
 
 
