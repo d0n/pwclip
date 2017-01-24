e9767aadc293fd7330a0d9c00fbf5ce4fd52f50d
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-01-24 01:27:26 +0100

    { Modified: [33mlib/net/mac.py[0m [33mmod[0m } [35mmaster[0m

--- mac.py
+++ mac.py
@@ -1,18 +1,20 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
+from struct import pack
+
 from net.iface import ifaces
 
 def mac(iface, byte=False):
     sock = socket(AF_INET, SOCK_DGRAM)
     info = ioctl(
         sock.fileno(), 0x8927,
-        struct.pack('256s', bytes(iface[:15], 'utf-8')))
+        pack('256s', bytes(iface[:15], 'utf-8')))
     if byte:
         smac = ''.join(['%02x'%c for c in info[18:24]])
         mac = b''
         for i in range(0, 12, 2):
             m = int(smac[i:i+2], 16)
-            mac += struct.pack('!B', m)
+            mac += pack('!B', m)
     else:
         mac = ':'.join(['%02x'%c for c in info[18:24]])
     return mac
