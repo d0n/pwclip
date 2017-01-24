8f613991d706c2d18ada1c4ecd91745240334fa8
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-01-25 00:28:10 +0100

    {'modified': ['lib/repo/git/sync.py']} master

--- git/sync.py
+++ git/sync.py
@@ -79,10 +79,11 @@ class GitSync(GitRepo):
 				self.gittreepush(
                     tree, '%s/%s.git'%(tremote, basename(tree)))
 			try:
-				print('%s/.git/subtree-cache'%getcwd())
-				#rmtree('%s/.git/subtree-cache'%self.rpodir)
+				#print('%s/.git/subtree-cache'%getcwd())
+				rmtree('%s/.git/subtree-cache'%self.rpodir)
 			except FileNotFoundError as err:
-				error(err)
+				pass
+				#error(err)
 
 	def giter(self, repos, syncall=None):
 		if self.dbg:
@@ -111,11 +112,6 @@ class GitSync(GitRepo):
 				rpobranchstats[repo] = stats
 			if self.tsy:
 				rpobranchstats[repo]['treesync'] = self.treesync()
-		try:
-			print('%s/.git/subtree-cache'%getcwd())
-			#rmtree('%s/.git/subtree-cache'%self.rpodir)
-		except FileNotFoundError as err:
-			error(err)
 		return rpobranchstats
 
 if __name__ == '__main__':
