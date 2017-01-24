66f5efcdf3d4992bd0157f854f0705b24f700853
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-01-24 01:36:12 +0100

    {'modified': ['lib/colortext/__init__.py', 'mod'], 'deleted': ['lib/colortext/debugger.py'], 'added': ['lib/colortext/logger.py']} master

--- __init__.py
+++ __init__.py
@@ -3,5 +3,4 @@ from colortext.colortext import \
     blu, cya, gre, grn, red, sys, vio, whi, yel, \
     bblu, bcya, bgre, bgrn, bred, bvio, bwhi, byel, \
     tabd, tabl, tabs, abort, error, fatal
-
-from colortext.debugger import logger, debug
+from colortext.logger import logger
