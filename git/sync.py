5453c1d94000840fefa54d674c1b34cb29c5df4c
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-02-01 18:12:13 +0100

    {'modified': ['lib/repo/git/sync.py']} master

--- git/sync.py
+++ git/sync.py
@@ -71,7 +71,7 @@ class GitSync(GitRepo):
 		trees = self.gitsubtrees(tremote)
 		if trees and self.remote:
 			self.gitsync()
-			print(yel(getcwd()), blu('syncing subtrees:'))
+			print(yel(getcwd()), blu('subtrees:'))
 			for tree in trees:
 				if tree == 'lib':
 					continue
