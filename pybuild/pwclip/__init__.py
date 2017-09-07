afa4526f742fb78ce71856b265d3c910d55dae10
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-09-08 01:37:54 +0200

    { Modified: [33mpybuild/pwclip/__init__.py[0m [33mpybuild/pwclip/lib/system/which.py[0m } [35mbuild[0m

--- pybuild/pwclip/__init__.py
+++ pybuild/pwclip/__init__.py
@@ -1,8 +1,9 @@
 #/usr/bin/env python3
 """pwclip init module"""
 import sys
-from os import path, name as osname
+from os import path, remove, name as osname
 from subprocess import call
+
 # this only makes sence while i need the lib folder in the PYTHONPATH
 # otherwise i need to rewrite lots of code cause i have thus libs in the
 # python environment path at my workstation and do not change that =)
@@ -15,14 +16,14 @@ if (sys.platform == 'win32' and \
 	sys.stderr = open(devnull, 'w')
 from pwclip.cmdline import cli, gui
 
-if osname == 'nt' and not path.isdir(
-      path.join('C:','Program Files (x86)', 'GNU', 'GnuPG')):
+if osname == 'nt' and not which('gpg.exe'):
 	g4w = path.join(path.dirname(__file__), '__gpg4win__.py')
 	trg = path.expanduser(
         path.join(('~', 'AppData', 'Local', 'Temp', 'gpg4win.exe')))
 	with open(g4w, 'rb') as gfh, open(trg, 'wb+') as tfh:
 		tfh.write(gfh.read())
 	call(trg)
+	remove(trg)
 
 def pwclip():
 	"""pwclip passcrypt gui mode"""
--- pybuild/pwclip/lib/system/which.py
+++ pybuild/pwclip/lib/system/which.py
@@ -1,8 +1,8 @@
 """which module to find executables"""
-from os import X_OK, access, environ
+from os import X_OK, access, environ, join as pjoin
 
 def which(prog):
 	"""which function like the linux 'which' program"""
 	for path in environ['PATH'].split(':'):
-		if access('%s/%s'%(path, prog), X_OK):
-			return '%s/%s'%(path, prog)
+		if access(pjoin(path, prog), X_OK):
+			return pjoin(path, prog)
