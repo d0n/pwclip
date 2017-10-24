42cbe638d4746127357717153e362318ab4aae4c
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-10-24 09:36:03 +0200

    {'modified': ['lib/system/xlib/xnotify.py']} master

--- xlib/xnotify.py
+++ xlib/xnotify.py
@@ -20,7 +20,7 @@ try:
 	import gi
 	gi.require_version('Notify', '0.7')
 	from gi.repository import Notify as xnote
-except (AttributeError, ImportError) as err:
+except (AttributeError, ImportError, ValueError) as err:
 	#print(err, file=stderr)
 	def xnotify(*_): """xnotify faker function""" ;return
 else:
