6045a377e9f759b6aaf0f99e135b2d29754f0508
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-10-24 06:42:15 +0200

    { Deleted: [33mpybuild/CHANGELOG.rst[0m [33mpybuild/DEPENDS[0m [33mpybuild/README[0m [33mpybuild/README.rst[0m [33mpybuild/pwclip.egg-info/SOURCES.txt[0m [33mpybuild/pwclip.egg-info/requires.txt[0m [33mpybuild/pwclip/__init__.py[0m [33mpybuild/pwclip/__main__.py[0m [33mpybuild/pwclip/__pkginfo__.py[0m [33mpybuild/pwclip/cmdline.py[0m [33mpybuild/pwclip/lib/__init__.py[0m [33mpybuild/pwclip/lib/colortext/.gitignore[0m [33mpybuild/pwclip/lib/colortext/__init__.py[0m [33mpybuild/pwclip/lib/colortext/colortext.py[0m [33mpybuild/pwclip/lib/net/.gitignore[0m [33mpybuild/pwclip/lib/net/__init__.py[0m [33mpybuild/pwclip/lib/net/dns.py[0m [33mpybuild/pwclip/lib/net/ssh.py[0m [33mpybuild/pwclip/lib/secrecy/.gitignore[0m [33mpybuild/pwclip/lib/secrecy/__init__.py[0m [33mpybuild/pwclip/lib/secrecy/gpg.py[0m [33mpybuild/pwclip/lib/secrecy/passcrypt.py[0m [33mpybuild/pwclip/lib/secrecy/yubi.py[0m [33mpybuild/pwclip/lib/system/.gitignore[0m [33mpybuild/pwclip/lib/system/__init__.py[0m [33mpybuild/pwclip/lib/system/clips.py[0m [33mpybuild/pwclip/lib/system/user/__init__.py[0m [33mpybuild/pwclip/lib/system/user/find.py[0m [33mpybuild/pwclip/lib/system/user/whoami.py[0m [33mpybuild/pwclip/lib/system/which.py[0m [33mpybuild/pwclip/lib/system/xlib/__init__.py[0m [33mpybuild/pwclip/lib/system/xlib/xinput.py[0m [33mpybuild/pwclip/lib/system/xlib/xmsgok.py[0m [33mpybuild/pwclip/lib/system/xlib/xnotify.py[0m [33mpybuild/pwclip/lib/system/xlib/xyesno.py[0m [33mpybuild/setup.py[0m } [35mmaster[0m

--- pybuild/pwclip/lib/system/xlib/__init__.py
+++
@@ -1,5 +0,0 @@
-"""system.xlib module"""
-from system.xlib.xinput import xinput
-from system.xlib.xmsgok import xmsgok
-from system.xlib.xyesno import xyesno
-from system.xlib.xnotify import xnotify
