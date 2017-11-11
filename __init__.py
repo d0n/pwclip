3cc0ca7fe4a145b550ebcdde13acbccd695f15e5
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-11-11 23:02:36 +0100

    {'deleted': ['.gitignore', '__init__.py', 'colortext.py', 'logger.py']} master

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""init of colortext just import all usability functions"""
-from colortext.colortext import \
-    blu, cya, gre, grn, red, vio, whi, yel, \
-    bblu, bcya, bgre, bgrn, bred, bvio, bwhi, byel, \
-    tabd, tabl, tabs, abort, error, fatal
-from colortext.logger import logger
