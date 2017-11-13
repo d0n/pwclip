558d991df433131d3e0c425e4e4e023369099ef8
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-11-13 21:07:23 +0100

    { Added: [33m.gitignore[0m [33m__init__.py[0m [33mcolortext.py[0m [33mlogger.py[0m } [35mmaster[0m

---
+++ logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+#from rainbow_logging_handler import RainbowLoggingHandler
+
+def logger(name, lvl='info'):
+	# 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+	lvls = {
+      'debug': logging.DEBUG,
+      'info': logging.INFO,
+      'warning': logging.WARNING,
+      'error': logging.ERROR,
+      'critical': logging.CRITICAL}
+	_logger = logging.getLogger(name)
+	_logger.setLevel(lvl if lvl in lvls.values() else lvls[lvl])
+	formats = logging.Formatter("%(message)s")  # same as default
+	handler = logging.FileHandler(name)
+	handler.setFormatter(formats)
+	_logger.addHandler(handler)
+	return _logger
