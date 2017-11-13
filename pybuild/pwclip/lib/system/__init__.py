c629ca9f735a338dd92a6786dc7f5867d2d1e1a4
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-11-14 00:11:56 +0100

    {'modified': ['pybuild/pwclip/lib/system/__init__.py'], 'deleted': ['pybuild/pwclip/lib/system/path.py']} master

--- pybuild/pwclip/lib/system/__init__.py
+++ pybuild/pwclip/lib/system/__init__.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-=======
-from system.stamp import stamp
-
-from system.fileage import fileage
-
-from system.path import absrelpath, realpaths, \
-    confpaths, confdats, jconfdats, filesiter, findupperdir
-
-from system.blockdevice import BlockDevice
-
-from system.sysfs import SysFs
-
-from system.bestlim import bestlim
-
-from system.lineno import lineno
-
-from system.which import which
-
-from system.random import random
-
-from system.string2bool import string2bool
-
-from system.gpgagent import agentinfo
-
->>>>>>> cbea4b204a1fa77e6f5c9cd00e50dd9b7ef9f530
 from system.clips import clips, copy, paste
 
 from system.user import userfind, whoami
