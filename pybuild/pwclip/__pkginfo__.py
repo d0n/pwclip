e085470e4db274895903be1185ae0ef3ed586ba2
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2017-11-17 03:06:53 +0100

    { Modified: [33mpybuild/pwclip/__pkginfo__.py[0m } [35mbuild[0m

--- pybuild/pwclip/__pkginfo__.py
+++ pybuild/pwclip/__pkginfo__.py
@@ -3,7 +3,7 @@
 from os import getcwd, path
 
 name = distname = modname = 'pwclip'
-numversion = (1, 1, 8)
+numversion = (1, 1, 9)
 version = '.'.join([str(num) for num in numversion])
 provides = ['pwcli', 'pwclip', 'ykclip']
 install_requires = [
