e2f6b56379a98dcdde0ef0600ebb6453b913ccb5
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-03-09 00:34:27 +0100

    {'modified': ['agentinfo.py', 'yamlazar.py']} master

--- agentinfo.py
+++ agentinfo.py
@@ -10,9 +10,4 @@ def gpgagentinfo(user=None):
 	gpgsock, sshsock = '%s/S.gpg-agent'%rundir, '%s/S.gpg-agent.ssh'%rundir
 	environ['GPG_AGENT_INFO'] = '%s:0:1'%gpgsock
 	environ['SSH_AUTH_SOCK'] = sshsock
-	if (
-          not issock(osstat(gpgsock).st_mode) or \
-          not issock(osstat(sshsock).st_mode)):
-		del environ['SSH_AUTH_SOCK']
-		sshsock = None
 	return gpgsock, sshsock
--- yamlazar.py
+++ yamlazar.py
@@ -20,12 +20,14 @@ from sys import stderr
 from os import \
     symlink, getcwd, listdir, \
     makedirs, symlink, walk, uname, chdir, \
-    remove, readlink, environ, chmod, stat, utime
+    remove, readlink, environ, chown, chmod, stat, utime
 
 from os.path import \
     abspath, isdir, islink, isfile, \
     dirname, expanduser, exists, basename, \
     join as pjoin
+
+from pathlib import Path
 
 from shutil import rmtree, move
 
@@ -34,8 +36,6 @@ from yaml import load, dump
 from colortext import blu, yel, bgre, tabd, error
 
 from system import absrelpath, fileage, filerotate
-
-from secrecy import GPGTool
 
 class Yamlazar():
 	""""Yamlazar"""
@@ -57,6 +57,24 @@ class Yamlazar():
 			print(' ', bgre(self.__init__))
 			print(bgre(tabd(self.__dict__, 4)))
 
+	def setmeta(self, path, metas):
+		_m = metas
+		try:
+			chown(path, _m['uid'], _m['gid'])
+		except PermissionError as err:
+			print(error)
+		chmod(path, _m['mod'])
+		utime(path, (_m['atm'], _m['mtm']))
+
+	def filemeta(self, path):
+		""" uid, gid, mod, mtm, atm, ctm """
+		s = list(stat(path))
+		return {
+            'uid': s[4], 'gid': s[5],
+            'mod': s[0], 'mtm': s[7], 'atm': s[8]}
+            #'st_uid': s[4], 'st_gid': s[5], 'st_mode': s[0],
+            #'st_mtime': s[7], 'st_atime': s[8], 'st_ctime': s[9]}
+
 	def path2dict(self, path):
 		if self.dbg:
 			print(bgre(self.path2dict))
@@ -66,22 +84,22 @@ class Yamlazar():
 		frbs['files'] = []
 		if not isdir(path): return
 		for (d, ss, fs) in walk(path):
-			frbs['dirs'].append({d: stat(d)})
+			frbs['dirs'].append((d, self.filemeta(d)))
 			for l in ss:
 				l  = pjoin(d, l)
 				if islink(l):
-					frbs['links'].append({l: readlink(l)})
+					frbs['links'].append((l, readlink(l)))
 			for f in fs:
 				if f == 'random_seed':
 					continue
 				f = pjoin(d, f)
 				if islink(f):
-					frbs['links'].append({f: readlink(f)})
+					frbs['links'].append((f, readlink(f)))
 					continue
 				try:
 					with open(f, 'rb') as rbf:
 						rb = rbf.read()
-					frbs['files'].append({f: [rb, stat(f)]})
+					frbs['files'].append((f, rb, self.filemeta(f)))
 				except OSError:
 					pass
 		return frbs
@@ -89,12 +107,34 @@ class Yamlazar():
 	def dict2path(self, dic):
 		if self.dbg:
 			print(bgre(self.dict2path))
-		print(tabd(dic))
+		if not dic: return False
+		dirs = dic['dirs']
+		files = dic['files']
+		links = dic['links']
+		for dss in dirs:
+			d, ss = dss
+			try:
+				makedirs(d)
+			except FileExistsError:
+				pass
+			self.setmeta(d, ss)
+		for fbs in files:
+			f, b, s = fbs
+			with open(f, 'wb+') as bfh:
+				bfh.write(b)
+		_pwd = getcwd()
+		for lns in links:
+			ln, trg = lns
+			chdir(dirname(ln))
+			symlink(ln, trg)
+			chdir(_pwd)
+
 
 	def create(self, path, ymla=None):
 		ymla = ymla if ymla else '%s.yla'%path
 		with open(ymla, 'wb+') as yfh:
 			yfh.write(str(dump(self.path2dict(path))).encode())
+
 
 	def extract(self, ymla, path=None):
 		path = path if path else dirname(ymla)
