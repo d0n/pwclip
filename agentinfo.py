910615caf4d2b43ad37f060c6b79e5268e7b67dc
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-03-11 15:08:17 +0100

    {'modified': ['agentinfo.py']} master

--- agentinfo.py
+++ agentinfo.py
@@ -1,13 +1,12 @@
 from os import environ, stat as osstat
 
-from stat import S_ISSOCK as issock
-
 from system.user import userfind
 
 def gpgagentinfo(user=None):
 	uid = int(userfind(userfind(), 'uid'))
 	rundir = '/run/user/%d/gnupg'%uid
-	gpgsock, sshsock = '%s/S.gpg-agent'%rundir, '%s/S.gpg-agent.ssh'%rundir
-	environ['GPG_AGENT_INFO'] = '%s:0:1'%gpgsock
+	#gpgsock, sshsock = '%s/S.gpg-agent'%rundir, '%s/S.gpg-agent.ssh'%rundir
+	gpgsock, sshsock = '%s/S.gpg-agent:0:1'%rundir, '%s/S.gpg-agent.ssh'%rundir
+	environ['GPG_AGENT_INFO'] = gpgsock
 	environ['SSH_AUTH_SOCK'] = sshsock
 	return gpgsock, sshsock
