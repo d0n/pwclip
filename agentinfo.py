ba50fb7c618fd331b5869b4fd7d9ed4f6903b93b
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-03-12 22:19:39 +0100

    {'modified': ['agentinfo.py']} master

--- agentinfo.py
+++ agentinfo.py
@@ -6,15 +6,13 @@ from system.user import userfind
 
 def gpgagentinfo(user=None):
 	uid = int(userfind(userfind(), 'uid'))
-	usrhome = userfind(uid, 'home')
-	gpgsock = '%s/.gnupg/S.gpg-agent'%usrhome
-	sshsock = '%s/.gnupg/S.gpg-agent.ssh'%usrhome
-	if (
-          not issock(osstat(gpgsock).st_mode) or \
-          not issock(osstat(sshsock).st_mode)):
-		rundir = '/run/user/%d/gnupg'%uid
-		gpgsock = '%s/S.gpg-agent'%rundir
-		sshsock = '%s/S.gpg-agent.ssh'%rundir
+	rundir = '/run/user/%d/gnupg'%uid
+	gpgsock = '%s/S.gpg-agent'%rundir
+	sshsock = '%s/S.gpg-agent.ssh'%rundir
+	if not issock(osstat(gpgsock).st_mode):
+		usrhome = userfind(uid, 'home')
+		gpgsock = '%s/.gnupg/S.gpg-agent'%usrhome
+		sshsock = '%s/.gnupg/S.gpg-agent.ssh'%usrhome
 	environ['GPG_AGENT_INFO'] = gpgsock
 	environ['SSH_AUTH_SOCK'] = sshsock
 	return gpgsock, sshsock
