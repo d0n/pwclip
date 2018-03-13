467ceef30e632f43f00cd68608db2e1a456e1c94
Leon Pelzer (lpelzer@lpunit) <leon.pelzer@1und1.de>
2018-03-13 15:40:56 +0100

    {'modified': ['agentinfo.py']} master

--- agentinfo.py
+++ agentinfo.py
@@ -1,15 +1,20 @@
-from os import environ, stat as osstat
+from os import \
+    getuid, environ, \
+    getenv, stat as osstat
+from os.path import exists
 
 from stat import S_ISSOCK as issock
 
 from system.user import userfind
 
 def gpgagentinfo(user=None):
-	uid = int(userfind(userfind(), 'uid'))
-	rundir = '/run/user/%d/gnupg'%uid
+	uid = getenv('SUDO_UID')
+	if not uid:
+		uid = getuid()
+	rundir = '/run/user/%s/gnupg'%uid
 	gpgsock = '%s/S.gpg-agent'%rundir
 	sshsock = '%s/S.gpg-agent.ssh'%rundir
-	if not issock(osstat(gpgsock).st_mode):
+	if not exists(gpgsock) or not issock(osstat(gpgsock).st_mode):
 		usrhome = userfind(uid, 'home')
 		gpgsock = '%s/.gnupg/S.gpg-agent'%usrhome
 		sshsock = '%s/.gnupg/S.gpg-agent.ssh'%usrhome
