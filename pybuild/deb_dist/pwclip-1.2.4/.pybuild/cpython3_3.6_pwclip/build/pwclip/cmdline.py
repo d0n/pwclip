77f44a10a16282706bfd8949ef3a3f58b5c07f84
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-03-27 07:39:11 +0200

    {'modified': ['pybuild/README', 'pybuild/pwclip/cmdline.py', 'pybuild/pwclip/lib'], 'added': ['pybuild/deb_dist/', 'pybuild/pwclip.egg-info/']} build

--- pybuild/deb_dist/pwclip-1.2.4/.pybuild/cpython3_3.6_pwclip/build/pwclip/cmdline.py
+++ pybuild/deb_dist/pwclip-1.2.4/.pybuild/cpython3_3.6_pwclip/build/pwclip/cmdline.py
@@ -170,7 +170,9 @@ def cli():
 	prol = 'pwclip - multi functional password manager to temporarily ' \
            'save passphrases  to your copy/paste buffers for easy and ' \
            'secure accessing your passwords'
-	pars = ArgumentParser(description=prol) #add_help=False)
+	epic = 'the yubikey feature is compatible with challenge-response ' \
+           'features only'
+	pars = ArgumentParser(description=prol, epilog=epic) #add_help=False)
 	pars.set_defaults(**cfgs)
 	pars.add_argument(
         '--version',
@@ -195,7 +197,7 @@ def cli():
         dest='time', default=3, metavar='seconds', type=int,
         help='time to wait before resetting clip (default is 3 max 3600)')
 
-	rpars = pars.add_argument_group('remote arguments')
+	rpars = pars.add_argument_group('remote')
 	rpars.add_argument(
         '-R',
         dest='rem', action='store_true',
@@ -209,7 +211,7 @@ def cli():
         dest='reuser', metavar='USER',
         help='use USER for connections to HOST')
 
-	gpars = pars.add_argument_group('gpg/ssl arguments')
+	gpars = pars.add_argument_group('gnupg/ssl')
 	gpars.add_argument(
         '-r', '--recipients',
         dest='rcp', metavar='ID(s)',
@@ -252,13 +254,13 @@ def cli():
         dest='ysl', default=2, type=int, choices=(1, 2),
         help='set one of the two slots on the yubi-key (only useful for -y)')
 
-	ypars = pars.add_argument_group('yubikey arguments')
+	ypars = pars.add_argument_group('yubikey')
 	ypars.add_argument(
         '-y', '--ykserial',
         nargs='?', dest='yks', metavar='SERIAL', default=False,
         help='switch to yubikey mode and optionally set SERIAL of yubikey')
 
-	gpars = pars.add_argument_group('actions')
+	gpars = pars.add_argument_group('passcrypt')
 	gpars.add_argument(
         '-a', '--add',
         dest='add', metavar='ENTRY',
---
+++ pybuild/deb_dist/pwclip_1.2.4-1.dsc
@@ -0,0 +1,19 @@
+Format: 3.0 (quilt)
+Source: pwclip
+Binary: python3-pwclip
+Architecture: all
+Version: 1.2.4-1
+Maintainer: Leon Pelzer <mail@leonpelzer.de>
+Standards-Version: 3.9.6
+Build-Depends: dh-python, python3-setuptools, python3-all, debhelper (>= 9)
+Package-List:
+ python3-pwclip deb python optional arch=all
+Checksums-Sha1:
+ 09f2b5cd55bdbc2da9f2e2944786075cff823bf9 27749 pwclip_1.2.4.orig.tar.gz
+ 7b8cc850a7a2b2e2976c00b2c103fbf08b38aac9 1376 pwclip_1.2.4-1.debian.tar.xz
+Checksums-Sha256:
+ 8695e0b5ae113fd0b43604bb3b45e5bcf94a48a3e160667d4af7cdf19a3dd54d 27749 pwclip_1.2.4.orig.tar.gz
+ aa56891f8e2610d357307cc5bf3445c6c8723a7a8ef51fbf8b40787ca65ac94d 1376 pwclip_1.2.4-1.debian.tar.xz
+Files:
+ ad39e9a836012bc0b5b5d5f219390de8 27749 pwclip_1.2.4.orig.tar.gz
+ b8eb59aa8738449124ca75f2b4dff45d 1376 pwclip_1.2.4-1.debian.tar.xz
