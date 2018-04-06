1156757db824ec45fca1942e1663de851b8ef806
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-04-06 02:30:37 +0200

    { Deleted: [33m.gitmodules[0m [33mlib[0m [33mpwclip/__init__.py[0m [33mpwclip/__main__.py[0m [33mpwclip/__pkginfo__.py[0m [33mpwclip/cmdline.py[0m [33mpwclip/lib/__init__.py[0m [33mpwclip/lib/colortext/.gitignore[0m [33mpwclip/lib/colortext/__init__.py[0m [33mpwclip/lib/colortext/colortext.py[0m [33mpwclip/lib/executor/.gitignore[0m [33mpwclip/lib/executor/__init__.py[0m [33mpwclip/lib/executor/executor.py[0m [33mpwclip/lib/net/.gitignore[0m [33mpwclip/lib/net/__init__.py[0m [33mpwclip/lib/net/dns.py[0m [33mpwclip/lib/net/ssh.py[0m [33mpwclip/lib/secrecy/.gitignore[0m [33mpwclip/lib/secrecy/__init__.py[0m [33mpwclip/lib/secrecy/gpgtools.py[0m [33mpwclip/lib/secrecy/passcrypt.py[0m [33mpwclip/lib/secrecy/yubi.py[0m [33mpwclip/lib/system/.gitignore[0m [33mpwclip/lib/system/__init__.py[0m [33mpwclip/lib/system/agentinfo.py[0m [33mpwclip/lib/system/clips.py[0m [33mpwclip/lib/system/path.py[0m [33mpwclip/lib/system/random.py[0m [33mpwclip/lib/system/user/__init__.py[0m [33mpwclip/lib/system/user/find.py[0m [33mpwclip/lib/system/user/whoami.py[0m [33mpwclip/lib/system/which.py[0m [33mpwclip/lib/system/xlib/__init__.py[0m [33mpwclip/lib/system/xlib/xinput.py[0m [33mpwclip/lib/system/xlib/xmsgok.py[0m [33mpwclip/lib/system/xlib/xnotify.py[0m [33mpwclip/lib/system/xlib/xyesno.py[0m [33msetup.py[0m } [35mbuild[0m

--- pwclip/lib/system/xlib/__init__.py
+++
@@ -1,5 +0,0 @@
-"""x window related functions"""
-from system.xlib.xinput import xinput, xgetpass
-from system.xlib.xmsgok import xmsgok
-from system.xlib.xyesno import xyesno
-from system.xlib.xnotify import xnotify
