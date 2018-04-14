ea8024b8fd2907ddc002a9acbb6e4efe755cbeea
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-04-14 16:21:40 +0200

    { Modified: [33m__pkginfo__.py[0m } [35mbuild[0m

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -43,3 +43,4 @@ entry_points = {
     'gui_scripts': ['pwclip = pwclip.__init__:pwclip',
                     'ykclip = pwclip.__init__:ykclip'],
     'console_scripts': ['pwcli = pwclip.__init__:pwcli']}
+data_files=[('docs/pwclip.1', ['man/man1/pwclip.1'])]
