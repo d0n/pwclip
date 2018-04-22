0ff8f06bb56702094963d4a90cf19948844a6773
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-04-22 23:28:39 +0200

    {'modified': ['cmdline.py']} build

--- cmdline.py
+++ cmdline.py
@@ -340,7 +340,7 @@ def cli():
 					if len(__pc) == 2:
 						xnotify('%s: %s'%(
                             args.lst, ' '.join(__pc[1:])), args.time)
-					prc = Popen(str('xvkbd -no-keypad -delay 1 -text "%s"'%__pc[0]).split(' '), stdout=PIPE, stderr=DEVNULL)
+					prc = Popen(str('xvkbd -no-keypad -xsendevent -delay 100 -text "%s"'%__pc[0]).split(' '), stdout=PIPE, stderr=DEVNULL, shell=True)
 					prc.communicate()
 					copy(__pc[0], 'pb')
 					forkwaitclip(__pc[0], poclp, boclp, args.time)
@@ -364,7 +364,7 @@ def cli():
 					if len(__pc) == 2:
 						xnotify('%s: %s'%(__in, __pc[1:]), args.time)
 					if args.out:
-						prc = Popen(str('xvkbd -no-keypad -delay 1 -text "%s"'%__pc[0]).split(' '), stdout=PIPE, stderr=DEVNULL)
+						prc = Popen(str('xvkbd -no-keypad -xsendevent -delay 100 -text "%s"'%__pc[0]).split(' '), stdout=PIPE, stderr=DEVNULL, shell=True)
 						prc.communicate()
 					copy(__pc[0], 'pb')
 					forkwaitclip(__pc[0], poclp, boclp, args.time)
@@ -400,7 +400,7 @@ def gui(typ='pw'):
 				xnotify('%s: %s'%(__in, __pc[1]), cfgs['time'])
 			poclp, boclp = paste('pb')
 			if 'out' in cfgs.keys():
-				prc = Popen(str('xvkbd -no-keypad -delay 1 -text "%s"'%__pc[0]).split(' '), stdout=PIPE, stderr=DEVNULL)
+				prc = Popen(str('xvkbd -no-keypad -xsendevent -delay 100 -text "%s"'%__pc[0]).split(' '), stdout=PIPE, stderr=DEVNULL, shell=True)
 			forkwaitclip(__pc[0], poclp, boclp, cfgs['time'])
 
 
