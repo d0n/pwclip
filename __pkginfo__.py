bfecc4b35cfc1aa20ffbdcf0ff260261be9a737d
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-04-24 21:21:57 +0200

    {'modified': ['__pkginfo__.py']} build

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """pwclip packaging information"""
 name = 'pwclip'
-version = '.'.join([str(num) for num in (1, 3, 1)])
+version = '.'.join([str(num) for num in (1, 3, 2)])
 provides = ['pwcli', 'pwclip', 'ykclip']
 install_requires = [
     'argcomplete', 'netaddr', 'paramiko', 'psutil',
     'pyusb', 'python-gnupg', 'python-yubico', 'PyYAML', 'wget']
 license = 'GPL'
 description = "gui to temporarily save passwords to system-clipboard"
+url = 'https://pypi.org/project/pwclip/%s/'%version
 author = 'Leon Pelzer'
 author_email = 'mail@leonpelzer.de'
 download_url = 'https://pypi.python.org/pypi/pwclip/%s#downloads'%version
@@ -28,16 +29,16 @@ classifiers = ['Environment :: Console',
                'Topic :: System :: Systems Administration']
 include_package_data = True
 try:
-	with open('pwclip/docs/CHANGELOG.rst', 'r') as cfh:
-		__changes = '\n\n\n'.join(cfh.read().split('\n\n\n')[:4])
+	long_description = '\n\n\n'.join(
+        str(open('pwclip/docs/CHANGELOG.rst', 'r').read()).split('\n\n\n')[:4])
 except OSError:
-	__changes = ''
+	long_description = ''
 try:
-	with open('pwclip/docs/README.rst', 'r') as rfh:
-		__readme = rfh.read().format(ChangeLog=__changes)
+	long_description = str(
+        open('pwclip/docs/README.rst', 'r').read()
+        ).format(ChangeLog=long_description)
 except OSError:
-	__readme = ''
-long_description = (__readme)
+	long_description = ''
 entry_points = {
     'gui_scripts': ['pwclip = pwclip.__init__:pwclip',
                     'ykclip = pwclip.__init__:ykclip'],
