3e30c1f92abf3b9bf4730580b634bd2c48ff86d0
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-04-25 00:33:39 +0200

    {'modified': ['cmdline.py']} build

--- cmdline.py
+++ cmdline.py
@@ -154,8 +154,8 @@ def confpars(mode):
            'save passphrases to your copy/paste buffers for easy and ' \
            'secure accessing your passwords. Most of the following ' \
            'arguments mights also be set by the config ~/.config/%s.yaml'%_me
-	epic = 'the yubikey feature is compatible with challenge-response ' \
-           'features only'
+	epic = 'the yubikey feature is compatible with yubikey\'s ' \
+           'challenge-response feature only'
 	pars = ArgumentParser(description=desc ,epilog=epic)
 	pars.set_defaults(**cfgs)
 	pars.add_argument(
