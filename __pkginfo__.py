2522ae82f4c6ebf441b94e0c4ab3b7fa774f4f80
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-04-30 20:16:31 +0200

    {'modified': ['__pkginfo__.py']} build

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -32,7 +32,7 @@ long_description = ''
 try:
 	with open('pwclip/docs/conf.py.tmpl', 'r') as tfh, \
           open('pwclip/docs/conf.py', 'w+') as cfh:
-		cfh.write(tfh.read().format(VersionString=version))
+		cfh.write(str(str(tfh.read()).format(VersionString=version)))
 except FileNotFoundError:
 	long_description = ''
 try:
