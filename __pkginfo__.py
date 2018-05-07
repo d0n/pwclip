8fbf2245de8f9fcf0e10f80b78a61fb3c1f6a4cd
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-05-08 00:33:14 +0200

    {'modified': ['__pkginfo__.py']} build

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -8,7 +8,7 @@ url = 'https://pypi.org/project/pwclip/'
 download_url = 'http://deb.janeiskla.de/python/python3-pwclip_current_all.deb'
 license = "GPLv3+"
 author = 'Leon Pelzer'
-description = 'password-manager that temporarily saves passwords into clipboard'
+description = 'password-manager that temporarily saves passwords in clipboard'
 author_email = 'mail@leonpelzer.de'
 classifiers = ['Environment :: Console',
                'Environment :: MacOS X',
