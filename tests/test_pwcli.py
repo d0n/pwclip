3b7e48c1f98ced34867199d43931a7eb891bda9d
d0n (d0n@d0nmachine) <d0n@janeiskla.de>
2018-05-11 03:47:11 +0200

    {'modified': ['setup.cfg', 'tests/test_pwcli.py']} build

--- tests/test_pwcli.py
+++ tests/test_pwcli.py
@@ -1,11 +1,9 @@
+"""
 import sys
 from unittest import TestCase
 from pwclip.cmdline import argspars, confargs, cli
 
 class CommandLineTestCase(TestCase):
-	"""
-	Base TestCase class, sets up a CLI parser
-	"""
 	@classmethod
 	def setUpClass(cls):
 		cls.parser = argspars('cli')
@@ -14,14 +12,9 @@ class CommandLineTestCase(TestCase):
 
 class TestCase4pwcli(CommandLineTestCase):
 	def test_with_empty_args():
-		"""
-		User passes no args, should fail with SystemExit
-		"""
 		with self.assertRaises(SystemExit):
 			self.parser.parse_args([])
 
 	def test_list_entrys():
-		"""
-		Find database servers with the Ubuntu AMI in Australia region
-		"""
 		cli(confsargs())
+"""
