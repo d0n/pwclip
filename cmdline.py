bf479c0ed0ac2cfb32a0878bbc8c5e4cec57b779
d0n <d0n@janeiskla.de>
2019-07-11 02:31:16 +0200

    modified = ['cmdline.py']

--- cmdline.py
+++ cmdline.py
@@ -57,6 +57,7 @@ def forkwaitclip(text, poclp, boclp, wait=3, out=None, enter=None):
 	if fork() == 0:
 		if enter:
 			text = '%s\n'%text
+		print(text)
 		if out == 'gui':
 			cmd.call('xvkbd -no-keypad -delay 20 -text "%s"'%text)
 		elif out == 'cli':
